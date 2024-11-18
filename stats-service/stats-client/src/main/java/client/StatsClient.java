1d28a21a8c24a57d6a979fd78be0cd392b0fb32f
Sergey Drozdov <SSDrozdov@alfabank.ru>
2024-11-19 00:39:36 +0300

    + диплом часть 2

--- stats-service/stats-client/src/main/java/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/client/StatsClient.java
@@ -11,7 +11,7 @@ public class StatsClient {
 
     public void addHit(RequestDto requestDto) {
         HttpEntity<RequestDto> request = new HttpEntity<>(requestDto);
-        restTemplate.postForObject("http://localhost:9090/hit", request, String.class);
-//        restTemplate.postForObject("http://stats-server:9090/hit", request, String.class);
+//        restTemplate.postForObject("http://localhost:9090/hit", request, String.class);
+        restTemplate.postForObject("http://stats-server:9090/hit", request, String.class);
     }
 }
