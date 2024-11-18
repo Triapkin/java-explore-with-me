d00f8fe27efa7a5e9e6a866f07b733e35e1d4974
Sergey Drozdov <SSDrozdov@alfabank.ru>
2024-11-18 22:36:34 +0300

    + диплом часть 2

--- main-service/src/main/java/ru/practicum/mainservice/exception/CheckTimeException.java
+++ main-service/src/main/java/ru/practicum/mainservice/exception/CheckTimeException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class CheckTimeException extends RuntimeException{
-    public CheckTimeException(String message){
+public class CheckTimeException extends RuntimeException {
+    public CheckTimeException(String message) {
         super(message);
     }
 }
